8e3dc4f13b38126ad00cbfb592aa5bd69a76cce8
sandraskolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>
2022-02-16 18:35:21 +0000

    Add css module to word component and style it

--- word-project/src/components/Word/Word.js
+++ word-project/src/components/Word/Word.js
@@ -1,8 +1,12 @@
+import css from "./Word.module.css";
+
 export default function Word({ word, date }) {
   return (
-    <>
-      <p>{date}</p>
-      <h3>{word}</h3>
-    </>
+    <div className={css.container}>
+      <div className={css.word}>
+        <p>{date}</p>
+        <h3>{word}</h3>
+      </div>
+    </div>
   );
 }
